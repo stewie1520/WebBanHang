062c33793c179809ba76086cc56d35e7be2ad7ae
Hieu Dong <donghuuhieu1520@gmail.com>
2020-11-11 09:38:27 +0700

    Fix response service

--- Models/ServiceResponse.cs
+++ Models/ServiceResponse.cs
@@ -9,7 +9,7 @@ namespace WebBanHang.Models
     {
         public T Data { get; set; }
         public bool Success { get; set; } = true;
-        public string? Message { get; set; } = null;
+        public string Message { get; set; } = null;
         public ErrorCode Code { get; set; } = ErrorCode.NONE; // indicate that no error
     }
 }
