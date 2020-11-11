be6dd83a738c27a095c5af04099a29ab6e978cb2
Hieu Dong <donghuuhieu1520@gmail.com>
2020-11-11 11:05:25 +0700

    WIP: implement helper function to upload image

---
+++ Helpers/AwsS3BucketHelper/AwsS3BucketHelper.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHang.Helpers.AwsS3BucketHelper
+{
+    public class AwsS3BucketHelper : IAwsS3BucketHelper
+    {
+        private readonly IAmazonS3 _amazonS3;
+        public AwsS3BucketHelper(IAmazonS3 amazonS3)
+        {
+            _amazonS3 = amazonS3;
+        }
+        public async Task<bool> UploadFile(Stream inputStream, string fileName)
+        {
+            try
+            {
+                PutObjectRequest request = new PutObjectRequest()
+                {
+                    InputStream = inputStream,
+                    BucketName = "",
+                    Key = fileName,
+                };
+
+                PutObjectResponse response = await _amazonS3.PutObjectAsync(request);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Helpers/AwsS3BucketHelper/IAwsS3BucketHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHang.Helpers.AwsS3BucketHelper
+{
+    public interface IAwsS3BucketHelper
+    {
+        Task<bool> UploadFile(System.IO.Stream inputStream, string fileName);
+    }
+}
