5de7db41ab636fab2ec9b29cdff73ac37e64b75c
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-01 09:59:13 +0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -39,7 +39,8 @@ namespace WebBanHang
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true);
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
             Configuration = builder.Build();
         }
