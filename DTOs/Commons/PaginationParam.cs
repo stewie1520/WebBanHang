6664096e74e2343d539d4769fe5451cab1e28f4d
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-05 00:06:01 -0700

    Add query extension

--- DTOs/Commons/PaginationParam.cs
+++ DTOs/Commons/PaginationParam.cs
@@ -6,5 +6,15 @@ namespace WebBanHang.DTOs.Commons
     {
         public int Page { get; set; } = 1;
         public int PerPage { get; set; } = BaseService.DefaultPerPage;
+
+        public string SortBy { get; set; }
+
+        public OrderByType OrderBy { get; set; } = OrderByType.ASC;
+        
+        private enum OrderByType
+        {
+            ASC,
+            DESC,
+        }
     }
 }
---
+++ Extensions/Query/IQueryableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHang.Extensions.Query
+{
+    public static class IQueryableExtension
+    {
+        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, Expression<Func<T, object>> keySelector, bool ascending)
+        {
+            var selectorBody = keySelector.Body;
+
+            if (selectorBody.NodeType == ExpressionType.Convert)
+                selectorBody = ((UnaryExpression)selectorBody).Operand;
+
+            var selector = Expression.Lambda(selectorBody, keySelector.Parameters);
+
+            var queryBody = Expression.Call(typeof(Queryable),
+                ascending ? "OrderBy" : "OrderByDescending",
+                new Type[] { typeof(T), selectorBody.Type },
+                source.Expression, Expression.Quote(selector));
+
+            return source.Provider.CreateQuery<T>(queryBody);
+        }
+    }
+}
