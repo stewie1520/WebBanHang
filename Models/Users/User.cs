da79a77da4d6b463fdef2d73af127eeab8f9aeba
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-13 14:33:24 +0000

    Add address, phone number of user
    Signed-off-by: GitHub <noreply@github.com>

--- Models/Users/User.cs
+++ Models/Users/User.cs
@@ -5,21 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebBanHang.Models
 {
-    public class User : BaseModel, ISoftDelete, IIdentity
-    {
-        [Required]
-        public string Avatar { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required, EmailAddress]
-        public string Email { get; set; }
-        public byte[] Password { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        [Column(TypeName = "date")]
-        public DateTime BirthDate { get; set; }
-        public Gender Gender { get; set; } = Gender.Unknown;
-        public IEnumerable<UserRole> UserRoles { get; set; }
-        [Required]
-        public bool IsDeleted { get; set; } = false;
-    }
+  public class User : BaseModel, ISoftDelete, IIdentity
+  {
+    [Required]
+    public string Avatar { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    public byte[] Password { get; set; }
+    public byte[] PasswordSalt { get; set; }
+    [Column(TypeName = "date")]
+    public DateTime BirthDate { get; set; }
+    public Gender Gender { get; set; } = Gender.Unknown;
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public IEnumerable<UserRole> UserRoles { get; set; }
+    [Required]
+    public bool IsDeleted { get; set; } = false;
+  }
 }
