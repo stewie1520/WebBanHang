442af24eb00fe333f3dfac8ca445c9eb07f19b73
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-13 13:24:40 +0000

    Fix exposed header
    Signed-off-by: GitHub <noreply@github.com>

--- Startup.cs
+++ Startup.cs
@@ -35,135 +35,135 @@ using WebBanHang.Services.WarehouseTransactionItem;
 
 namespace WebBanHang
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IWebHostEnvironment env)
     {
-        public Startup(IWebHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+          .AddEnvironmentVariables();
 
-            Configuration = builder.Build();
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            services.AddScoped<IAuthorizationService<User>, AuthorizationService<User>>();
-            services.AddScoped<IAuthorizationService<Customer>, AuthorizationService<Customer>>();
-            services.AddScoped<IFileUploaderService, FileUploaderService>();
-            services.AddScoped<ICategoriesService, CategoriesService>();
-            services.AddScoped<IProductService, ProductService>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IWarehouseTransactionService, WarehouseTransactionService>();
-            services.AddScoped<IWarehouseTransactionItemService, WarehouseTransactionItemService>();
-
-            services.AddCors(options => options.AddPolicy("EasePolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader();
-            }));
-
-            services.AddControllers()
-                .ConfigureApiBehaviorOptions(opt =>
-                {
-                    opt.InvalidModelStateResponseFactory = ValidateModelState.InvalidModelState;
-                });
-
-            services.AddDbContext<DataContext>(optionBuilder =>
-                optionBuilder.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
-
-            services.AddAutoMapper(typeof(Startup));
-
-            services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("WebBanHang", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Api đồ án website bán hàng",
-                    Description = "Api document của đồ án website bán hàng, happy coding!",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Hieu the developer",
-                        Email = "donghuuhieu1520@gmail.com",
-                    }
-                });
-
-                var xmlFilePath = Path.Combine(AppContext.BaseDirectory,
-                    $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
-
-                options.IncludeXmlComments(xmlFilePath);
-
-                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                });
-
-                options.AddRequiredAuthorizationHeader();
-            });
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(Configuration["AppSettings:SecretKey"])),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                    };
-
-                    options.Events = new JwtBearerEvents
-                    {
-                        OnAuthenticationFailed = context =>
-                        {
-                            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-                            {
-                                context.Response.Headers.Add("Token-Expired", "true");
-                            }
-                            return Task.CompletedTask;
-                        }
-                    };
-                });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseCors("EasePolicy");
-
-            app.UseSwagger();
-            app.UseSwaggerUI(setupAction =>
-            {
-                setupAction.SwaggerEndpoint("/swagger/WebBanHang/swagger.json", "WebBanHang");
-            });
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = builder.Build();
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+      services.AddScoped<IAuthorizationService<User>, AuthorizationService<User>>();
+      services.AddScoped<IAuthorizationService<Customer>, AuthorizationService<Customer>>();
+      services.AddScoped<IFileUploaderService, FileUploaderService>();
+      services.AddScoped<ICategoriesService, CategoriesService>();
+      services.AddScoped<IProductService, ProductService>();
+      services.AddScoped<IUserService, UserService>();
+      services.AddScoped<IWarehouseTransactionService, WarehouseTransactionService>();
+      services.AddScoped<IWarehouseTransactionItemService, WarehouseTransactionItemService>();
+
+      services.AddCors(options => options.AddPolicy("EasePolicy", builder =>
+      {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithExposedHeaders("Token-Expired");
+      }));
+
+      services.AddControllers()
+          .ConfigureApiBehaviorOptions(opt =>
+          {
+            opt.InvalidModelStateResponseFactory = ValidateModelState.InvalidModelState;
+          });
+
+      services.AddDbContext<DataContext>(optionBuilder =>
+          optionBuilder.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+
+      services.AddAutoMapper(typeof(Startup));
+
+      services.AddSwaggerGen(options =>
+      {
+        options.SwaggerDoc("WebBanHang", new OpenApiInfo
+        {
+          Version = "v1",
+          Title = "Api đồ án website bán hàng",
+          Description = "Api document của đồ án website bán hàng, happy coding!",
+          Contact = new OpenApiContact
+          {
+            Name = "Hieu the developer",
+            Email = "donghuuhieu1520@gmail.com",
+          }
+        });
+
+        var xmlFilePath = Path.Combine(AppContext.BaseDirectory,
+                  $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
+
+        options.IncludeXmlComments(xmlFilePath);
+
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+          In = ParameterLocation.Header,
+          Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+          Name = "Authorization",
+          Type = SecuritySchemeType.ApiKey,
+        });
+
+        options.AddRequiredAuthorizationHeader();
+      });
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = new SymmetricSecurityKey(
+                          Encoding.UTF8.GetBytes(Configuration["AppSettings:SecretKey"])),
+              ValidateIssuer = false,
+              ValidateAudience = false,
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+              OnAuthenticationFailed = context =>
+                    {
+                      if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                      {
+                        context.Response.Headers.Add("Token-Expired", "true");
+                      }
+                      return Task.CompletedTask;
+                    }
+            };
+          });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseCors("EasePolicy");
+      app.UseSwagger();
+      app.UseSwaggerUI(setupAction =>
+      {
+        setupAction.SwaggerEndpoint("/swagger/WebBanHang/swagger.json", "WebBanHang");
+      });
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
