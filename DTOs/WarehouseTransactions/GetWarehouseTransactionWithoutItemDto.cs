6b0e126187088df20921a3d39359d900b8425129
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-24 16:18:11 +0000

    Fix api get all warehouse transaction
    Signed-off-by: GitHub <noreply@github.com>

--- DTOs/WarehouseTransactions/GetWarehouseTransactionWithoutItemDto.cs
+++ DTOs/WarehouseTransactions/GetWarehouseTransactionWithoutItemDto.cs
@@ -5,14 +5,15 @@ using WebBanHang.Models;
 
 namespace WebBanHang.DTOs.WarehouseTransactions
 {
-    public class GetWarehouseTransactionWithoutItemDto
-    {
-        public int Id { get; set; }
-        [Required]
-        public WarehouseTransactionType TransactionType { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public WarehouseTransactionStatus Status { get; set; }
-        public int CreatedBy { get; set; }
-        public string Description { get; set; }
-    }
+  public class GetWarehouseTransactionWithoutItemDto
+  {
+    public int Id { get; set; }
+    [Required]
+    public WarehouseTransactionType TransactionType { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public WarehouseTransactionStatus Status { get; set; }
+    public Manufacturer Manufacturer { get; set; }
+    public int CreatedBy { get; set; }
+    public string Description { get; set; }
+  }
 }
---
+++ Migrations/20201224154133_fixWarehouseTransation.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebBanHang.Migrations
+{
+    public partial class fixWarehouseTransation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManufacturerId",
+                table: "WarehouseTransactions",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WarehouseTransactions_ManufacturerId",
+                table: "WarehouseTransactions",
+                column: "ManufacturerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WarehouseTransactions_Manufacturers_ManufacturerId",
+                table: "WarehouseTransactions",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WarehouseTransactions_Manufacturers_ManufacturerId",
+                table: "WarehouseTransactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WarehouseTransactions_ManufacturerId",
+                table: "WarehouseTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "ManufacturerId",
+                table: "WarehouseTransactions");
+        }
+    }
+}
