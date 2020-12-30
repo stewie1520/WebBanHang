72af5fa3657a6d2d5ef089b054fbee4595f7f267
Hieu Dong <donghuuhieu1520@gmail.com>
2020-12-30 04:18:22 +0000

    Fix api get all basket
    Signed-off-by: GitHub <noreply@github.com>

--- DTOs/Baskets/GetBasketWithoutItemDto.cs
+++ DTOs/Baskets/GetBasketWithoutItemDto.cs
@@ -1,24 +1,26 @@
 using System.Collections.Generic;
 using System;
 using System.ComponentModel.DataAnnotations;
+
 using WebBanHang.DTOs.BasketItems;
 using WebBanHang.Models;
+using WebBanHang.DTOs.Customers;
 
 namespace WebBanHang.DTOs.Baskets
 {
-    public class GetBasketWithoutItemDto
-    {
-        [Required]
-        public int Id { get; set; }
-        [Required]
-        public bool IsPaid { get; set; }
-        public BasketStatus Status { get; set; } = BasketStatus.Ordering;
-        [Required]
-        public int? CustomerId { get; set; }
-        [Required]
-        public string Note { get; set; }
-        [Required]
-        public int TotalPrice { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
+  public class GetBasketWithoutItemDto
+  {
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public bool IsPaid { get; set; }
+    public BasketStatus Status { get; set; } = BasketStatus.Ordering;
+    [Required]
+    public GetCustomerDto Customer { get; set; }
+    [Required]
+    public string Note { get; set; }
+    [Required]
+    public int TotalPrice { get; set; }
+    public DateTime UpdatedAt { get; set; }
+  }
 }
